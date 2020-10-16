6a8976708493fe4379d51a58da9d4217cfc8519e
Kiyomi Sugita <ksugi006@wch136-24.cs.ucr.edu>
2020-10-16 00:45:12 -0700

    Completed part 4

---
+++ turnin/ksugi006_lab2_part4.c
@@ -0,0 +1,46 @@
+/*	Author: ksugi006
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab 2  Exercise 2
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0x00; PORTB = 0xFF;
+	DDRC = 0x00; PORTC = 0x00;
+	DDRD = 0xFF; PORTD = 0x00;
+	
+	unsigned char totWeight = 0x00;
+	unsigned short difWeight = 0x00;
+	unsigned char tmp = 0x00;
+    /* Insert your solution below */
+    while (1) {
+	
+	totWeight = PINA + PINB + PINC;
+	totWeight = totWeight & 0xFC;
+	
+	tmp = totWeight;
+
+	if(totWeight > 0x8C){
+		tmp = tmp + 0x01; //setting PD0 to 1 if > 140
+	}
+
+	difWeight = PINA - PINC + 50;
+
+	if((difWeight < 0x00) || (difWeight > 0x64)){
+		tmp = tmp + 0x02; //setting PD1 to 1 if inbetween 50 and -50
+	}
+
+	PORTD = tmp;
+   }	
+    return 0;
+}
---
+++ turnin/ksugi006_lab2_part4_tests.py
@@ -0,0 +1,43 @@
+# Array of tests to run (in order)
+# Each test contains
+#   description - 
+#   steps - A list of steps to perform, each step can have
+#       inputs - A list of tuples for the inputs to apply at that step
+#       *time - The time (in ms) to wait before continuing to the next step 
+#           and before checking expected values for this step. The time should be a multiple of
+#           the period of the system
+#       *iterations - The number of clock ticks to wait (periods)
+#       expected - The expected value at the end of this step (after the "time" has elapsed.) 
+#           If this value is incorrect the test will fail early before completing.
+#       * only one of these should be used
+#   expected - The expected output (as a list of tuples) at the end of this test
+# An example set of tests is shown below. It is important to note that these tests are not "unit tests" in 
+# that they are not ran in isolation but in the order shown and the state of the device is not reset or 
+# altered in between executions (unless preconditions are used).
+tests = [ {'description': 'PINA: 0x00 PINB: 0x00 PINC: 0x00 => PORTC: 00000000 = 0x00',
+    'steps': [ {'inputs': [('PINA',0x00), ('PINB', 0x00), ('PINC', 0x00)], 'iterations': 1 } ],
+    'expected': [('PORTD',0x00)],
+    },
+	{'description': 'PINA = 50 PINB: 50 PINC: 50 => PORTC: 150 = 10010101 = 0x95',
+	'steps': [{'inputs': [('PINA', 0x32), ('PINB', 0x32), ('PINC', 0x32)], 'iterations': 1} ],
+	'expected': [('PORTD', 0x95)],
+	},
+        {'description': 'PINA = 90 PINB: 50 PINC: 10 => PORTC: 150 = 10010111 = 0x97',
+        'steps': [{'inputs': [('PINA', 0x5A), ('PINB', 0x32), ('PINC', 0x0A)], 'iterations': 1} ],
+        'expected': [('PORTD', 0x97)],
+        },
+        {'description': 'PINA = 40 PINB: 50 PINC: 40 => PORTC: 130 = 10000000 = 0x80',
+        'steps': [{'inputs': [('PINA', 0x28), ('PINB', 0x32), ('PINC', 0x28)], 'iterations': 1} ],
+        'expected': [('PORTD', 0x80)],
+        },
+        {'description': 'PINA = 80 PINB: 50 PINC: 00 => PORTC: 130 = 10000010 = 0x82',
+        'steps': [{'inputs': [('PINA', 0x50), ('PINB', 0x32), ('PINC', 0x00)], 'iterations': 1} ],
+        'expected': [('PORTD', 0x82)],
+        },
+]
+
+# Optionally you can add a set of "watch" variables these need to be global or static and may need
+# to be scoped at the function level (for static variables) if there are naming conflicts. The 
+# variables listed here will display everytime you hit (and stop at) a breakpoint
+#watch = ['<function>::<static-var>','PORTB']
+
